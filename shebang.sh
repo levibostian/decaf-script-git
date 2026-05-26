31f8b4e8afaaa555e51b5bf51cd8af4183e6a8b3
Levi Bostian <levi@levi.earth>
2026-05-26 07:22:08 -0500

    setup decaf shebang script
    - Created a script to be run by 'decaf shebang' command, used to run the script
    - Delete jsr and npm code because no longer needed.

---
+++ shebang.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Script that runs via decaf's shebang feature.
+
+REPO="levibostian/decaf-script-git"
+
+if [ "$DECAF_SHEBANG_REF" = "tag" ]; then
+  # if shebang ran for a tag, download pre-built binary and run it. 
+  URL="https://github.com/$REPO/releases/download/$DECAF_SHEBANG_REF_NAME/install.sh"
+
+  curl -fsSL "$URL" | sh
+  $HOME/.local/bin/decaf-script-git "$@"
+else
+  # else, run via deno. 
+  deno task --quiet run "$@"
+fi
