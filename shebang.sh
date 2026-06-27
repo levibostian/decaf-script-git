7bcb03c3c4d575a20ad74b6093b62a6fd25d48aa
Levi Bostian <levi@levi.earth>
2026-06-27 07:37:10 -0500

    fix: change to using the new mise shebang feature
    no need to compile binaries.
    run script via decaf.

--- shebang.sh
+++ shebang.sh
@@ -1,17 +1,3 @@
 #!/usr/bin/env bash
-set -eu
 
-# Script that runs via decaf's shebang feature.
-
-REPO="levibostian/decaf-script-git"
-
-if [ "$DECAF_SHEBANG_REF" = "tag" ]; then
-  # if shebang ran for a tag, download pre-built binary and run it. 
-  URL="https://github.com/$REPO/releases/download/$DECAF_SHEBANG_REF_NAME/install.sh"
-
-  curl -fsSL "$URL" | sh
-  $HOME/.local/bin/decaf-script-git "$@"
-else
-  # else, run via deno. 
-  deno task --quiet run "$@"
-fi
+mise exec -- deno task --quiet run "$@"
